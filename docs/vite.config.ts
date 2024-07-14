5f3b823ea0de2c9e281ebafc4672bed6044caf81
Jannik Maag <55883716+fuzzypawzz@users.noreply.github.com>
2024-07-14 18:38:24 +0200

    Prepare documentation

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   root: './docs',
+  base: '/Model-Vue-Presenter/',
   build: {
     outDir: '../dist/docs',
     rollupOptions: {
