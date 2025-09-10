1e3eaabd73eea444408f949358b96395e02c9229
Jannik Maag <55883716+fuzzypawzz@users.noreply.github.com>
2025-09-10 18:22:15 +0200

    #2 Return fallback viewModel preventing Vue components from crashing and triggering error boundaries instead

--- lib/presenter-factory.ts
+++ lib/presenter-factory.ts
@@ -1,14 +1,16 @@
-import { computed } from 'vue'
+import { computed, getCurrentScope, onMounted } from 'vue'
+
+import { ERROR } from '@/constants/error'
+import { tryOnScopeDispose } from '@/helpers/try-on-scope-dispose'
 import type {
-    PresenterConfig, PresenterFactoryFunction,
-    PresenterHookOutput,
-    Props,
-    View,
-    ViewModelOverride
-} from "@/presenter-factory.types";
-import { validatePresenterConfig, validateViewModelOverride } from "@/validators";
-import { tryOnScopeDispose } from "@/helpers/try-on-scope-dispose";
-import { ERROR } from "@/constants/error";
+  PresenterConfig,
+  PresenterFactoryFunction,
+  PresenterHookOutput,
+  Props,
+  View,
+  ViewModelOverride
+} from '@/presenter-factory.types'
+import { validatePresenterConfig, validateViewModelOverride } from '@/validators'
 
 /**
  * @example
@@ -30,113 +32,141 @@ import { ERROR } from "@/constants/error";
  * const { presenter, viewModel } = useDocumentOverviewPresenter()
  */
 export function presenterFactory<
-    TPresenterOptions extends PresenterConfig,
-    TProps extends Props = void,
-    TView extends View = void
->(presenterFactoryFunction: PresenterFactoryFunction<TProps, TView, TPresenterOptions>)
-{
-    const cachedPresenterInstance = {
-        value: undefined as TPresenterOptions | undefined,
-        reset() {
-            this.value = undefined
-        }
+  TPresenterOptions extends PresenterConfig,
+  TProps extends Props = void,
+  TView extends View = void
+>(presenterFactoryFunction: PresenterFactoryFunction<TProps, TView, TPresenterOptions>) {
+  const cachedPresenterInstance = {
+    value: undefined as TPresenterOptions | undefined,
+    reset() {
+      this.value = undefined
     }
+  }
 
-    const viewModelOverride = {
-        value: undefined as ViewModelOverride<TPresenterOptions> | undefined,
-        reset() {
-            this.value = undefined
-        }
+  const viewModelOverride = {
+    value: undefined as ViewModelOverride<TPresenterOptions> | undefined,
+    reset() {
+      this.value = undefined
     }
+  }
 
-    function createPresenterProxy(
-        presenterOptions: TPresenterOptions,
-        viewModelOverride: ViewModelOverride<TPresenterOptions>
-    ) {
-        return new Proxy(presenterOptions, {
-            get(target, property: keyof PresenterConfig) {
-                if (property === 'viewModel') {
-                    return computed(() => viewModelOverride(target.viewModel.value))
-                }
+  function createPresenterProxy(
+    presenterOptions: TPresenterOptions,
+    viewModelOverride: ViewModelOverride<TPresenterOptions>
+  ) {
+    return new Proxy(presenterOptions, {
+      get(target, property: keyof PresenterConfig) {
+        if (property === 'viewModel') {
+          return computed(() => viewModelOverride(target.viewModel.value))
+        }
 
-                return Reflect.get(target, property)
-            }
+        return Reflect.get(target, property)
+      }
+    })
+  }
+
+  function createFallbackPresenterAndViewModel() {
+    return {
+      presenter: {},
+      viewModel: computed(() => {
+        return {}
+      })
+    } as PresenterHookOutput<TPresenterOptions>
+  }
+
+  /**
+   * @example
+   * const { viewModel, presenter } = useFooPresenter(props, view)
+   */
+  function usePresenterHook(props: TProps, view: TView): PresenterHookOutput<TPresenterOptions> {
+    let presenterApi: TPresenterOptions
+
+    try {
+      presenterApi = presenterFactoryFunction(props, view)
+    } catch (errorInPresenter) {
+      /**
+       * Prevent Vue component templates from breaking during render
+       * when they access the viewModel like this `my-attr="viewModel.someProperty"`.
+       * Else we would get errors "TypeError: Cannot read properties of undefined (reading 'someProperty')"
+       */
+      if (getCurrentScope()) {
+        onMounted(() => {
+          throw errorInPresenter
         })
+
+        return createFallbackPresenterAndViewModel()
+      }
+
+      throw errorInPresenter
     }
 
-    /**
-     * @example
-     * const { viewModel, presenter } = useFooPresenter(props, view)
-     */
-    function usePresenterHook(props: TProps, view: TView): PresenterHookOutput<TPresenterOptions> {
-        const config = presenterFactoryFunction(props, view)
+    validatePresenterConfig(presenterApi)
 
-        validatePresenterConfig(config)
+    if (viewModelOverride.value)
+      validateViewModelOverride(viewModelOverride.value, presenterApi.viewModel.value)
 
-        if (viewModelOverride.value) validateViewModelOverride(viewModelOverride.value, config.viewModel.value)
+    tryOnScopeDispose(() => {
+      presenterApi.onDestroy?.()
+      cachedPresenterInstance.reset()
+    })
 
-        tryOnScopeDispose(() => {
-            config.onDestroy?.()
-            cachedPresenterInstance.reset()
-        })
+    presenterApi.onCreated?.()
 
-        config.onCreated?.()
+    const presenterOptions = viewModelOverride.value
+      ? createPresenterProxy(presenterApi, viewModelOverride.value)
+      : presenterApi
 
-        const presenterOptions = viewModelOverride.value
-            ? createPresenterProxy(config, viewModelOverride.value)
-            : config
+    cachedPresenterInstance.value = presenterOptions
 
-        cachedPresenterInstance.value = presenterOptions
+    viewModelOverride.reset()
 
-        viewModelOverride.reset()
-
-        return {
-            presenter: presenterOptions,
-            viewModel: presenterOptions.viewModel
-        }
+    return {
+      presenter: presenterOptions,
+      viewModel: presenterOptions.viewModel
     }
+  }
 
-    /**
-     * @example
-     * const wrapper = mount(DocumentOverview)
-     * const { viewModel } = useDocumentOverviewPresenter.spy()
-     *
-     * expect(viewModel.value.amountOfClicks).toBe(0)
-     * wrapper.find('button').trigger('click')
-     * expect(viewModel.value.amountOfClicks).toBe(1)
-     */
-    usePresenterHook.spy = function (): PresenterHookOutput<TPresenterOptions> {
-        const presenterOptions = cachedPresenterInstance.value
+  /**
+   * @example
+   * const wrapper = mount(DocumentOverview)
+   * const { viewModel } = useDocumentOverviewPresenter.spy()
+   *
+   * expect(viewModel.value.amountOfClicks).toBe(0)
+   * wrapper.find('button').trigger('click')
+   * expect(viewModel.value.amountOfClicks).toBe(1)
+   */
+  usePresenterHook.spy = function (): PresenterHookOutput<TPresenterOptions> {
+    const presenterOptions = cachedPresenterInstance.value
 
-        if (!presenterOptions) throw ERROR.SPYING.NO_PRESENTER_INSTANCE
+    if (!presenterOptions) throw ERROR.SPYING.NO_PRESENTER_INSTANCE
 
-        cachedPresenterInstance.reset()
+    cachedPresenterInstance.reset()
 
-        return {
-            presenter: presenterOptions,
-            viewModel: presenterOptions.viewModel
-        }
+    return {
+      presenter: presenterOptions,
+      viewModel: presenterOptions.viewModel
     }
+  }
 
-    // TODO: resetSpy needs testing
-    usePresenterHook.resetSpy = function () {
-        cachedPresenterInstance.reset()
-    }
+  // TODO: resetSpy needs testing
+  usePresenterHook.resetSpy = function () {
+    cachedPresenterInstance.reset()
+  }
 
-    /**
-     * @example
-     * useDocumentOverviewPresenter.mockViewModel((viewModel) => {
-     *   return {
-     *     ...viewModel,
-     *     showPageSkeletonLoader: true,
-     *   }
-     * })
-     */
-    usePresenterHook.mockViewModel = function (
-        viewModelOverrideFunction: ViewModelOverride<TPresenterOptions>
-    ) {
-        viewModelOverride.value = viewModelOverrideFunction
-    }
+  /**
+   * @example
+   * useDocumentOverviewPresenter.mockViewModel((viewModel) => {
+   *   return {
+   *     ...viewModel,
+   *     showPageSkeletonLoader: true,
+   *   }
+   * })
+   */
+  usePresenterHook.mockViewModel = function (
+    viewModelOverrideFunction: ViewModelOverride<TPresenterOptions>
+  ) {
+    viewModelOverride.value = viewModelOverrideFunction
+  }
 
-    return usePresenterHook
+  return usePresenterHook
 }
